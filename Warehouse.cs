2227cfc27385c0697986cab68e50501890c1ced4
ostap1177 <103674333+ostap1177@users.noreply.github.com>
2024-03-30 20:30:34 +0300

    Create Warehouse.cs

---
+++ Warehouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Saw_UseCase
+{
+    public class Warehouse : Storage
+    {
+        public override string Paylink { get; protected set; }
+
+        public void Unload(Good good, int count)
+        {
+            if (Goods.TryGetValue(good, out int value))
+            {
+                if (value >= count)
+                {
+                    Goods[good] -= count;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+        }
+    }
+}
